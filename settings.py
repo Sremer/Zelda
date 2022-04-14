f5e73126e2b22b76b9bd503f0ee23ec7b15093d0
Sremer <seth.e.remer@gmail.com>
2022-04-14 13:46:43 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -36,5 +36,4 @@ magic_data = {
     'flame': {'strength': 5, 'cost': 20, 'graphic': 'graphics/particles/flame/fire.png'},
     'heal': {'strength': 20, 'cost': 10, 'graphic': 'graphics/particles/heal/heal.png'}}
 
-#test
-#will this actually work
+
