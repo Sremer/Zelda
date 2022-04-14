3daae8bc831e7743b358614cd8b9663d94bdd802
seth <seth.e.remer@gmail.com>
2022-04-14 13:40:10 -0600

    Testing pycharm commit

--- settings.py
+++ settings.py
@@ -35,3 +35,6 @@ weapon_data = {
 magic_data = {
     'flame': {'strength': 5, 'cost': 20, 'graphic': 'graphics/particles/flame/fire.png'},
     'heal': {'strength': 20, 'cost': 10, 'graphic': 'graphics/particles/heal/heal.png'}}
+
+#test
+#will this actually work
